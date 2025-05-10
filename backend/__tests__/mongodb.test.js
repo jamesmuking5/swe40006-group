100d085a328194bffc50648215b95b9377ed352c
james <jamesmuking@gmail.com>
2025-05-11 04:50:52 +0800

    added mongodb-memory-server as dependency for mock server in unit testing

---
+++ backend/__tests__/mongodb.test.js
@@ -0,0 +1,5 @@
+// File: backend/__test__/mongodb.test.js
+// Description: This file contains the test cases for the MongoDB connection logic in the car shop application.
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import {}

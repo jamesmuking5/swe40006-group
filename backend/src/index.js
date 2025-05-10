e8db5486fb856a15035ce0057dc34f3adeb30a0b
james <jamesmuking@gmail.com>
2025-05-11 04:32:37 +0800

    initialize mongodb connection before server starts, and added graceful server shutdowns

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 
 // Import environment variables from .env file and always refresh
 dotenv.config(".env", { override: true });
+const serviceLocation = "Main Service";
+
+// Import MongoDB connection function
+import { connectToMongoDB } from "./database/mongodb.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -15,7 +19,39 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to serve static files from the 'public' directory
 app.use(express.static("public"));
 
-// Listen on the specified port and host
-app.listen(PORT, HOST, () => {
-  console.log(`Server is running at http://${HOST}:${PORT}`);
-});
+// Add parentheses to immediately invoke the async function
+(async () => {
+  try {
+    // Connect to MongoDB
+    await connectToMongoDB();
+    console.log(`${serviceLocation}: MongoDB connection established`);
+
+    // Declare server variable
+    let server = app.listen(PORT, HOST, () => {
+      console.log(
+        `${serviceLocation}: Server is running at http://${HOST}:${PORT}`
+      );
+    });
+
+    // Gracefully handle server shutdown
+    const signals = ["SIGINT", "SIGTERM"];
+    signals.forEach((signal) => {
+      process.on(signal, async () => {
+        console.log(`${serviceLocation}: Received ${signal}. Shutting down...`);
+        server.close(() => {
+          console.log(`${serviceLocation}: Server closed`);
+          process.exit(0);
+
+          setTimeout(() => {
+            console.error(
+              `${serviceLocation}: Forcefully shutting down after timeout`
+            );
+            process.exit(1);
+          }, 5); // 5 seconds timeout
+        });
+      });
+    });
+  } catch (error) {
+    console.error(`${serviceLocation}: Error starting server: ${error}`);
+  }
+})();
